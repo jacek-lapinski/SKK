a4e4f6f22cdfbc25948852541bfd28ae493a7715
Jacek Łapiński <jacek.lapinski@ihs.com>
2016-10-04 16:53:12 +0200

    add new sections

--- gruntfile.js
+++ gruntfile.js
@@ -4,12 +4,36 @@ module.exports = function (grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
-        
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './app/styles',
+                    src: ['*.scss'],
+                    dest: './build/styles',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: './build/styles',
+                    src: ['*.css', '!*.min.css'],
+                    dest: './build/styles',
+                    ext: '.min.css'
+                }]
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask('build', [
-        
+        'sass', 'cssmin'
     ]);
 
     grunt.registerTask('default', ['build']);
