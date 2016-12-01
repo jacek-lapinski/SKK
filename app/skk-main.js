843f96284661635fd18cc8a7539d2efc0b59142e
Jacek Łapiński <jacek.michal.lapinski@outlook.com>
2016-12-01 11:39:52 +0100

    finishing

--- app/skk-main.js
+++ app/skk-main.js
@@ -1,74 +1,62 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        animateOut: 'fadeOut',
-        autoplay: true,
-        autoplayTimeout: 1500,
-        autoplayHoverPause: false
-    });
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 0,
+		animateOut: 'fadeOut',
+		autoplay: true,
+		autoplayTimeout: 1500,
+		autoplayHoverPause: false
+	});
 });
 
-
-$('#famax').famax({
-	appId: '852602131447787',
-	accessToken: 'EAAMHbZBctZCZBsBAJmWi3Pkr0ZCgBuDuzyJbFNZBZAddSB1eoaqlZA4XGWWKvEIvjzF4iC6RFZBE0ct82ZBpKUWbxHom4ZCy2UurvS2jAoYzjfEuaIlTpvtgpMZAk5Fe5FvJjZBi32vdZCh7F7DR4h18MC9G0WWLvmXIZBcW8ZD',
-	fanPage: 'http://www.facebook.com/kendosopot',
-
-	//NON MANDATORY
-	maxResults: 12, //Maximum videos to display in one load
-	innerOffset: 30, //Distance between video thumbnails
-	minItemWidth: 300, //Minumum width of a video thumbnails
-	maxItemWidth: 450, //Maximum width of a video thumbnails
-	outerOffset: 30, //Distance between video thumbnails and plugin conatiner
-	maxContainerWidth: 1000, //Maximum width of plugin container
-	alwaysUseDropdown: false,
-	notFoundImage: 'http://www.404notfound.fr/assets/images/pages/img/onsydney.jpg', //change this image link
-	maxAttachments: 8,
-	selectedTab: 'p',
-	skin: 'none',
-	displayMetricsForTags: false,
-	displayMetricsForPosts: false,
-	maxComments: 14,
-	onClickAction: 'popup',
-	refreshTimeout: 1000
+facebookGallery({
+	appId: '1270801512983487',
+	accessToken: '1270801512983487|21c21db8b582aa474f30bea9b73edc0b',
+	fbPage: 'kendosopot',
+	galleryOptions: {
+		elementId: 'fsg-galeria',
+		imagesCountLimit: 99,
+		excludeAlbums: ['Timeline Photos', 'Mobile Uploads', 'Cover Photos', 'Profile Pictures', 'Untitled Album']
+	},
+	postsOptions: {
+		elementId: 'fsg-news',
+		postsCountLimit: 12
+	}
 });
 
-$('#famax-galeria').famax({
-	appId: '852602131447787',
-	accessToken: 'EAAMHbZBctZCZBsBAJmWi3Pkr0ZCgBuDuzyJbFNZBZAddSB1eoaqlZA4XGWWKvEIvjzF4iC6RFZBE0ct82ZBpKUWbxHom4ZCy2UurvS2jAoYzjfEuaIlTpvtgpMZAk5Fe5FvJjZBi32vdZCh7F7DR4h18MC9G0WWLvmXIZBcW8ZD',
-	fanPage: 'http://www.facebook.com/kendosopot',
-
-	//NON MANDATORY
-	maxResults: 12, //Maximum videos to display in one load
-	innerOffset: 30, //Distance between video thumbnails
-	minItemWidth: 300, //Minumum width of a video thumbnails
-	maxItemWidth: 450, //Maximum width of a video thumbnails
-	outerOffset: 30, //Distance between video thumbnails and plugin conatiner
-	maxContainerWidth: 1000, //Maximum width of plugin container
-	alwaysUseDropdown: false,
-	notFoundImage: 'http://www.404notfound.fr/assets/images/pages/img/onsydney.jpg', //change this image link
-	maxAttachments: 8,
-	selectedTab: 'l',
-	skin: 'none',
-	displayMetricsForTags: false,
-	displayMetricsForPosts: false,
-	maxComments: 14,
-	onClickAction: 'popup',
-	refreshTimeout: 1000
+lightbox.option({
+	'alwaysShowNavOnTouchDevices': true,
+	'albumLabel': 'Zdjęcie %1 z %2',
+	'maxHeight': 500
 });
+
+$(function () {
+	$('#motto').textillate({
+		loop: false,
+		autoStart: true,
+		type: 'char',
+		in: {
+			effect: 'fadeInRight',
+			sync: false,
+			shuffle: false,
+			reverse: false,
+		},
+		out: {}
+	});
+})
 
 $(window).scroll(function (event) {
 	Scroll();
 });
 
-
-$('.slowscroll a').on('click', function (e) {
+$('.scrolling a').on('click', function (e) {
 	e.preventDefault();
 	$('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
 	return false;
 });
+
+new WOW().init();
 
 function Scroll() {
 	var contentTop = [];
@@ -84,8 +72,8 @@ function Scroll() {
 	$.each(contentTop, function (i) {
 		if (winTop > contentTop[i] - rangeTop) {
 			$('.navbar-collapse li.scroll')
-                .removeClass('active')
-                .eq(i).addClass('active');
+				.removeClass('active')
+				.eq(i).addClass('active');
 		}
 	})
 }
